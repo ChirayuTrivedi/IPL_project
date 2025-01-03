07942f93563399a5f4fcba95a25e3f1d144c5013
username <wecp-dummy@gmail.com>
2025-01-03 16:15:47 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/entity/Cricketer.java
+++ backend/src/main/java/com/wecp/progressive/entity/Cricketer.java
@@ -1,54 +1,62 @@
 package com.wecp.progressive.entity;
 
-public class Cricketer {
-    private int cricket_id;
-    private int team_id;
-    private String cricket_name;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Comparator;
+
+@Entity
+public class Cricketer implements Comparable<Cricketer> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cricketerId;
+    private int teamId;
+    private String cricketerName;
     private int age;
     private String nationality;
-    private int  experience;
+    private int experience;
     private String role;
-    private int total_run;
-    private int total_wicket;
-
-    public Cricketer(int cricket_id, int team_id, String cricket_name, int age, String nationality, int experience,
-            String role, int total_run, int total_wicket) {
-        this.cricket_id = cricket_id;
-        this.team_id = team_id;
-        this.cricket_name = cricket_name;
-        this.age = age;
-        this.nationality = nationality;
-        this.experience = experience;
-        this.role = role;
-        this.total_run = total_run;
-        this.total_wicket = total_wicket;
-    }
+    private int totalRuns;
+    private int totalWickets;
 
     public Cricketer() {
     }
 
-    public int getCricket_id() {
-        return cricket_id;
+    public Cricketer(int cricketerId, int teamId, String cricketerName, int age, String nationality, int experience, String role, int totalRuns, int totalWickets) {
+        this.cricketerId = cricketerId;
+        this.teamId = teamId;
+        this.cricketerName = cricketerName;
+        this.age = age;
+        this.nationality = nationality;
+        this.experience = experience;
+        this.role = role;
+        this.totalRuns = totalRuns;
+        this.totalWickets = totalWickets;
     }
 
-    public void setCricket_id(int cricket_id) {
-        this.cricket_id = cricket_id;
+    public int getCricketerId() {
+        return cricketerId;
     }
 
-    public int getTeam_id() {
-        return team_id;
+    public void setCricketerId(int cricketerId) {
+        this.cricketerId = cricketerId;
     }
 
-    public void setTeam_id(int team_id) {
-        this.team_id = team_id;
+    public int getTeamId() {
+        return teamId;
     }
 
-    public String getCricket_name() {
-        return cricket_name;
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
     }
 
-    public void setCricket_name(String cricket_name) {
-        this.cricket_name = cricket_name;
+    public String getCricketerName() {
+        return cricketerName;
+    }
+
+    public void setCricketerName(String cricketerName) {
+        this.cricketerName = cricketerName;
     }
 
     public int getAge() {
@@ -83,23 +91,25 @@ public class Cricketer {
         this.role = role;
     }
 
-    public int getTotal_run() {
-        return total_run;
+    public int getTotalRuns() {
+        return totalRuns;
     }
 
-    public void setTotal_run(int total_run) {
-        this.total_run = total_run;
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
     }
 
-    public int getTotal_wicket() {
-        return total_wicket;
+    public int getTotalWickets() {
+        return totalWickets;
     }
 
-    public void setTotal_wicket(int total_wicket) {
-        this.total_wicket = total_wicket;
+    public void setTotalWickets(int totalWickets) {
+        this.totalWickets = totalWickets;
     }
 
-    
-
-    
+    @Override
+    public int compareTo(Cricketer otherCricketer) {
+        return Comparator.comparingInt(Cricketer::getExperience)
+                .compare(this, otherCricketer);
+    }
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/TicketBookingServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TicketBookingServiceImpl.java
@@ -1,5 +1,34 @@
 package com.wecp.progressive.service.impl;
-
-public class TicketBookingServiceImpl  {
-
+ 
+import java.util.List;
+ 
+import com.wecp.progressive.dao.TeamDAO;
+import com.wecp.progressive.entity.Team;
+import com.wecp.progressive.service.TeamService;
+ 
+public class TicketBookingServiceImpl implements TeamService  {
+    private TeamDAO teamDAO;
+   
+ 
+    public TicketBookingServiceImpl(TeamDAO teamDAO) {
+        this.teamDAO = teamDAO;
+    }
+ 
+    @Override
+    public List<Team> getAllTeams() {
+        return null;
+       
+    }
+ 
+    @Override
+    public int addTeam(Team team) {
+        return -1;
+    }
+ 
+    @Override
+    public List<Team> getAllTeamsSortedByName() {
+        return List.of();
+    }
+   
+ 
 }
